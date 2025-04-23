b311445ac4b4308da5d0ae006070846a8605269d
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2025-04-22 22:55:36 -0300

    refactor(menu): reorder menu items for improved organization

--- components/menu.tsx
+++ components/menu.tsx
@@ -17,25 +17,6 @@ export function Menu() {
 
 
 
-      <MenuItem
-          icon="skull"
-          text="Amigos de mierda"
-          href="https://eugeniosaintemarie.github.io/amigos-de-mierda?ref=hub"
-        />
-        <MenuItem icon="dice" text="Generala tracker" href="https://generala-tracker.vercel.app/?ref=hub" />
-        <MenuItem icon="trophy" text="Baza tracker" href="https://baza-tracker.vercel.app/?ref=hub" />
-
-        <div className="h-[1px] bg-[#808080] my-1 mx-2"></div>
-
-        <MenuItem icon="notes" text="Notes" href="https://eugeniosaintemarie.github.io/notes?ref=hub" />
-        <MenuItem icon="quote" text="Quotes" href="https://eugeniosaintemarie.github.io/quotes?ref=hub" />
-        <MenuItem icon="camera" text="Photos" href="https://eugeniosaintemarie.github.io/photos?ref=hub" />
-
-        <MenuItem icon="pinterest" text="Pinterest" href="https://www.pinterest.es/eugeniosaintemarie" />
-        <MenuItem icon="music" text="Spotify" href="https://open.spotify.com/user/eugeniosaintemarie/playlists" />
-
-        <div className="h-[1px] bg-[#808080] my-1 mx-2"></div>
-
         <MenuItem icon="coffee" text="Coffeedoro" href="https://coffee-doro.vercel.app/?ref=hub" />
         <MenuItem icon="stopwatch" text="Pomodoro" href="https://pomodoro-esm.netlify.app/#/" />
         <MenuItem
@@ -43,6 +24,24 @@ export function Menu() {
           text="Number2WhatsApp"
           href="https://eugeniosaintemarie.github.io/whatsapp-chat?ref=hub"
         />
+
+        <div className="h-[1px] bg-[#c0c0c0] my-1 mx-2"></div>
+
+        <MenuItem
+            icon="skull"
+          text="Amigos de mierda"
+          href="https://eugeniosaintemarie.github.io/amigos-de-mierda?ref=hub"
+        />
+        <MenuItem icon="dice" text="Generala tracker" href="https://generala-tracker.vercel.app?ref=hub" />
+        <MenuItem icon="trophy" text="Baza tracker" href="https://baza-tracker.vercel.app?ref=hub" />
+
+        <div className="h-[1px] bg-[#808080] my-1 mx-2"></div>
+
+        <MenuItem icon="notes" text="Notes" href="https://eugeniosaintemarie.github.io/notes?ref=hub" />
+        <MenuItem icon="quote" text="Quotes" href="https://eugeniosaintemarie.github.io/quotes?ref=hub" />
+        <MenuItem icon="camera" text="Photos" href="https://eugeniosaintemarie.github.io/photos?ref=hub" />
+        <MenuItem icon="pinterest" text="Pinterest" href="https://www.pinterest.es/eugeniosaintemarie" />
+        <MenuItem icon="music" text="Spotify" href="https://open.spotify.com/user/eugeniosaintemarie/playlists" />
 
         <div className="h-[1px] bg-[#808080] my-1 mx-2"></div>
 
