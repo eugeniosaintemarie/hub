f7f5079ef4c2ae50e1b75c33181821c921c21ea8
Eugenio Sainte Marie <eugenio.saintemarie@protonmail.ch>
2025-04-28 12:10:28 -0300

    feat: add YearProgress component with custom props
    Introduce the YearProgress component to the main page and menu, allowing for custom title, start date, end date, and label. This enhances the user interface by providing a visual progress indicator with customizable options.

--- components/menu.tsx
+++ components/menu.tsx
@@ -7,58 +7,58 @@ export function Menu() {
   return (
     <MenuProvider>
       <div className="bg-[#c0c0c0] border-2 border-[#ffffff] border-r-[#808080] border-b-[#808080] shadow-lg w-fit">
-      <div className="border-r-2 border-[#808080] w-8 h-full absolute left-0 top-0 bg-[#818181] flex flex-col items-center pt-2">
-        <span
-          className="text-white font-bold text-lg writing-mode-vertical transform rotate-180"
-          style={{ writingMode: "vertical-rl" }}
-        >
-          ∃ugenio hub
-        </span>
-      </div>
+        <div className="border-r-2 border-[#808080] w-8 h-full absolute left-0 top-0 bg-[#818181] flex flex-col items-center pt-2">
+          <span
+            className="text-white font-bold text-lg writing-mode-vertical transform rotate-180"
+            style={{ writingMode: "vertical-rl" }}
+          >
+            ∃ugenio hub
+          </span>
+        </div>
 
-      <div className="pl-10 py-2 whitespace-nowrap w-fit">
-        <YearProgress />
-
+        <div className="pl-10 py-2 whitespace-nowrap w-fit">
 
 
-      <MenuGroup title="Tools">
-        <MenuItem icon="coffee" text="Coffeedoro" href="https://coffee-doro.vercel.app/?ref=hub" />
-        <MenuItem icon="stopwatch" text="Pomodoro" href="https://pomodoro-esm.netlify.app/#/" />
-        <MenuItem
-          icon="message"
-          text="Number2WhatsApp"
-          href="https://eugeniosaintemarie.github.io/whatsapp-chat?ref=hub"
-        />
-      </MenuGroup>
 
-      <MenuGroup title="Games">
-        <MenuItem
-          icon="skull"
-          text="Amigos de mierda"
-          href="https://eugeniosaintemarie.github.io/amigos-de-mierda?ref=hub"
-        />
-        <MenuItem icon="dice" text="Generala tracker" href="https://generala-tracker.vercel.app?ref=hub" />
-        <MenuItem icon="trophy" text="Baza tracker" href="https://baza-tracker.vercel.app?ref=hub" />
-      </MenuGroup>
 
-      <MenuItem icon="notes" text="Notes" href="https://eugeniosaintemarie.github.io/notes?ref=hub" />
-      <MenuItem icon="quote" text="Quotes" href="https://eugeniosaintemarie.github.io/quotes?ref=hub" />
-      <MenuItem icon="camera" text="Photos" href="https://eugeniosaintemarie.github.io/photos?ref=hub" />
-      <MenuItem icon="pinterest" text="Pinterest" href="https://www.pinterest.es/eugeniosaintemarie" />
-      <MenuItem icon="music" text="Spotify" href="https://open.spotify.com/user/eugeniosaintemarie/playlists" />
+          <MenuGroup title="Tools">
+            <MenuItem icon="coffee" text="Coffeedoro" href="https://coffee-doro.vercel.app/?ref=hub" />
+            <MenuItem icon="stopwatch" text="Pomodoro" href="https://pomodoro-esm.netlify.app/#/" />
+            <MenuItem
+              icon="message"
+              text="Number2WhatsApp"
+              href="https://eugeniosaintemarie.github.io/whatsapp-chat?ref=hub"
+            />
+          </MenuGroup>
 
-      <div className="h-[1px] bg-[#808080] my-1 mx-2"></div>
+          <MenuGroup title="Games">
+            <MenuItem
+              icon="skull"
+              text="Amigos de mierda"
+              href="https://eugeniosaintemarie.github.io/amigos-de-mierda?ref=hub"
+            />
+            <MenuItem icon="dice" text="Generala tracker" href="https://generala-tracker.vercel.app?ref=hub" />
+            <MenuItem icon="trophy" text="Baza tracker" href="https://baza-tracker.vercel.app?ref=hub" />
+          </MenuGroup>
 
-      { /*
+          <MenuItem icon="notes" text="Notes" href="https://eugeniosaintemarie.github.io/notes?ref=hub" />
+          <MenuItem icon="quote" text="Quotes" href="https://eugeniosaintemarie.github.io/quotes?ref=hub" />
+          <MenuItem icon="camera" text="Photos" href="https://eugeniosaintemarie.github.io/photos?ref=hub" />
+          <MenuItem icon="pinterest" text="Pinterest" href="https://www.pinterest.es/eugeniosaintemarie" />
+          <MenuItem icon="music" text="Spotify" href="https://open.spotify.com/user/eugeniosaintemarie/playlists" />
+
+          <div className="h-[1px] bg-[#808080] my-1 mx-2"></div>
+
+          { /*
       <MenuItem icon="portfolio" text="Portfolio" href="https://eugeniosaintemarie.github.io/portfolio?ref=hub" />
       */ }
 
-      <MenuItem icon="shutdown" text="Shut Down" onClick={() => window.close()} />
+          <MenuItem icon="shutdown" text="Shut Down" onClick={() => window.close()} />
 
 
 
+        </div>
       </div>
-    </div>
     </MenuProvider>
   )
 }
