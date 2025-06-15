35660bac2773fd3ad4f6aa81ea292ab96adda49d
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2025-06-15 17:35:34 -0300

    feat(layout): add font-awesome css cdn link for icon support

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <link rel="icon" href="https://eugeniosaintemarie.github.io/favicon.ico" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-LNQ47VG50M"></script>
         <script dangerouslySetInnerHTML={{
           __html: `
