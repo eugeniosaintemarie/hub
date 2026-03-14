88d642995df0a9edad5f577c5af57bb55c978f01
Eugenio Sainte Marie <eugenio.saintemarie@protonmail.com>
2026-03-13 22:28:40 -0300

    fix8

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css'
 export const metadata: Metadata = {
   title: '∃ugenio hub',
   description: '∃ugenio links',
+  viewport: 'width=device-width, initial-scale=1.0',
 }
 
 export default function RootLayout({
